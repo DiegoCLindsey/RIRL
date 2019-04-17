aadd8072749c223e88a5940e1341b26861fa6ed9
Diego <Diegoclindsey@gmail.com>
2019-04-17 16:34:54 +0200

    generic function sendObject()

--- app/src/main/java/com/cppandi/rirl/utils/firestoreUtils.java
+++ app/src/main/java/com/cppandi/rirl/utils/firestoreUtils.java
@@ -1,4 +1,27 @@
 package com.cppandi.rirl.utils;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class firestoreUtils {
+    public void sendObject(Object o,String path){
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection(path).add(o).addOnCompleteListener(
+                new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if (task.isSuccessful()) {
+                            Log.d("OK","Archivo subido con éxito");
+                        } else {
+                            Log.d("ERROR", "Error al subir el objeto", task.getException());
+                        }
+                    }
+                }
+        );
+    }
 }
