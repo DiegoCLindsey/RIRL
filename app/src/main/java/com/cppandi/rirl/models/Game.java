2e7908463011ceac07a5df374c560d88e1815374
Diego <Diegoclindsey@gmail.com>
2019-04-17 16:29:55 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- app/src/main/java/com/cppandi/rirl/models/Game.java
+++ app/src/main/java/com/cppandi/rirl/models/Game.java
@@ -1,5 +1,15 @@
 package com.cppandi.rirl.models;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 import java.util.Date;
 import java.util.List;
 
