647e0ea4504a612f9d973d478361d8a85d592614
Diego <diegoclindsey@gmail.com>
2019-05-18 21:06:28 +0200

    DBQuery
    Pruebas

---
+++ app/src/main/java/com/cppandi/rirl/utils/DBquerys.java
@@ -0,0 +1,37 @@
+package com.cppandi.rirl.utils;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class DBquerys<T> {
+    private Object response;
+
+    public DBquerys(String id,String collection){
+        FirebaseFirestore.getInstance().collection(collection).document(id).get().addOnCompleteListener(
+                new OnCompleteListener<DocumentSnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            final Object res = task.getResult().toObject(Object.class);
+                            response = res;
+
+                        } else {
+                            Log.d("prueba", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+    }
+
+    public T getResponse(){
+        return (T) response;
+    }
+
+}
